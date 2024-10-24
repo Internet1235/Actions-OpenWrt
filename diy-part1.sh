9dc33b0db9413fbaa0dbe3a8c9d0842c9e9ee864
Internet1235 <135099493+Internet1235@users.noreply.github.com>
2024-10-24 20:14:30 +0800

    🕊

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,7 +3,7 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-##
+#
 sed -i '/#src-git luci https:\/\/github.com\/coolsnowwolf\/luci$/s/^#//' feeds.conf.default && sed -i '/src-git luci https:\/\/github.com\/coolsnowwolf\/luci.git;openwrt-23.05$/s/^/#/' feeds.conf.default
 # Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
 #
