ca0b119f4eb621c6ac418bb1507fbe64d2a5da2f
Internet1235 <135099493+Internet1235@users.noreply.github.com>
2024-12-17 13:38:52 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,19 +12,19 @@ git clone https://github.com/Internet1235/openwrt-iptvhelper.git package/iptvhel
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
-echo "# Put your custom commands here that should be executed once
+#echo "# Put your custom commands here that should be executed once
 # the system init finished. By default this file does nothing.
 
 # 启动2.4g 和 5g 信号
-ip link set ra0 up
-ip link set rai0 up
+#ip link set ra0 up
+#ip link set rai0 up
 
 # 桥接网卡
-brctl addif br-lan ra0
-brctl addif br-lan rai0
+#brctl addif br-lan ra0
+#brctl addif br-lan rai0
 
 
-exit 0" > package/base-files/files/etc/rc.local
+#exit 0" > package/base-files/files/etc/rc.local
 
 # Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
 #
