e7d9024740fdf24946f141bf049c90ed4d5fe7e8
Internet1235 <135099493+Internet1235@users.noreply.github.com>
2026-01-18 01:30:11 +0800

    change rockchip-armv8

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,27 +4,27 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 rm -rf feeds/luci/themes/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/downloads/luci-theme-argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
-#git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci-light/Makefile
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/downloads/luci-theme-argon
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci-light/Makefile
 git clone https://github.com/Internet1235/openwrt-iptvhelper.git package/iptvhelper
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
-git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+#git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
-echo "# Put your custom commands here that should be executed once
+#echo "# Put your custom commands here that should be executed once
 # the system init finished. By default this file does nothing.
 
 # 启动2.4g 和 5g 信号
-ip link set ra0 up
-ip link set rai0 up
+#ip link set ra0 up
+#ip link set rai0 up
 
 # 桥接网卡
-brctl addif br-lan ra0
-brctl addif br-lan rai0
+#brctl addif br-lan ra0
+#brctl addif br-lan rai0
 
 
-exit 0" > package/base-files/files/etc/rc.local
+#exit 0" > package/base-files/files/etc/rc.local
 
 # Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
 #
